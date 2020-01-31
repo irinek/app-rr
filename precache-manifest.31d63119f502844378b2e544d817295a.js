189146a20dc0c68e0aa37c41a34a50e21c4fd942
irinek <irinek@o2.pl>
2020-01-31 21:09:42 +0100

    Updates

---
+++ precache-manifest.31d63119f502844378b2e544d817295a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "9873500cbc742282ea50",
+    "url": "/app-rr/static/js/runtime~main.9873500c.js"
+  },
+  {
+    "revision": "c2e722be4cc2fc85a5bc",
+    "url": "/app-rr/static/js/main.c2e722be.chunk.js"
+  },
+  {
+    "revision": "784d7767c79093c2e1eb",
+    "url": "/app-rr/static/js/2.784d7767.chunk.js"
+  },
+  {
+    "revision": "c2e722be4cc2fc85a5bc",
+    "url": "/app-rr/static/css/main.3177261c.chunk.css"
+  },
+  {
+    "revision": "784d7767c79093c2e1eb",
+    "url": "/app-rr/static/css/2.e7b26d11.chunk.css"
+  },
+  {
+    "revision": "4b8b25cf025d8136d11c17aae9704914",
+    "url": "/app-rr/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f23562e0cd53a7710130139a6a55d62.js"
+  "/app-rr/precache-manifest.31d63119f502844378b2e544d817295a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/app-rr/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
