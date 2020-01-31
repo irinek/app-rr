5dbad8d30ca6cfbc838dbdbd346e233e1a5d8789
irinek <irinek@o2.pl>
2020-01-31 21:04:49 +0100

    Updates

---
+++ precache-manifest.0f23562e0cd53a7710130139a6a55d62.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c2e722be4cc2fc85a5bc",
+    "url": "/static/js/main.c2e722be.chunk.js"
+  },
+  {
+    "revision": "784d7767c79093c2e1eb",
+    "url": "/static/js/2.784d7767.chunk.js"
+  },
+  {
+    "revision": "c2e722be4cc2fc85a5bc",
+    "url": "/static/css/main.3177261c.chunk.css"
+  },
+  {
+    "revision": "784d7767c79093c2e1eb",
+    "url": "/static/css/2.e7b26d11.chunk.css"
+  },
+  {
+    "revision": "10532c8ef54058e16e36d4ccef463027",
+    "url": "/index.html"
+  }
+];
